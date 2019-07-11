e147658626bace5d5b1080aa3462b40e44d49ee9
Senthilkumar <senthilkasiqa@gmail.com>
2019-07-11 09:09:31 +0530

    adding commit

--- src/test/java/com/lti/base/BaseDriver.java
+++ src/test/java/com/lti/base/BaseDriver.java
@@ -39,7 +39,7 @@ public class BaseDriver extends DataProviderClass {
 		DesiredCapabilities capability=new DesiredCapabilities();
 		capability = DesiredCapabilities.chrome();
 		capability.setPlatform(org.openqa.selenium.Platform.WINDOWS);
-		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		driver = new RemoteWebDriver(new URL("http://192.168.1.5:4444/wd/hub"), capability);
 		/*System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(90, TimeUnit.MILLISECONDS);*/
